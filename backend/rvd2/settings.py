3414b6f8c1dc566da0ff5726d3ae09a0b196c75e
Tyler Brinker <113711489+TSBrinker@users.noreply.github.com>
2023-01-12 21:17:47 -0600

    Update settings.py

--- backend/rvd2/settings.py
+++ backend/rvd2/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9hhl-j^e2)#3*im_givv^5_d1!o$dmn2renj+kn3t_@i7yqkk_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
