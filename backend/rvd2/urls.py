20b46e15f6a5feb8db6babb24bd2f6bb6b4206b9
TSBrinker <tsbrinker@gmail.com>
2023-01-28 23:33:55 -0600

    added frontend folder to project

--- backend/rvd2/urls.py
+++ backend/rvd2/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path, include
+
+
 """rvd2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +21,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/robots/', include ('robots.urls')),
+    path('api/dinosaurs/', include ('dinosaurs.urls')),
+    path('api/weapons/', include ('weapons.urls')),
 ]
